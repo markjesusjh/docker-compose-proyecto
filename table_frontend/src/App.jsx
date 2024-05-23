ab36ae8e4e0ff3428df6920c57885fc1eea05f8e
markjesusjh <143747566+markjesusjh@users.noreply.github.com>
2024-05-23 04:37:07 -0500

    Add files via upload

--- table_frontend/src/App.jsx
+++ table_frontend/src/App.jsx
@@ -1,3 +1,4 @@
+// App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
@@ -12,16 +13,21 @@ import './styles/App.css'
 const theme = createTheme();
 
 const App = () => {
+  // Manejar la redirección para el slash vacío
+  if (window.location.pathname === '/') {
+    window.location.href = '/home'; // Redirigir a la página de inicio o cualquier otra ruta
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <Router>
         <div>
           <NavBar />
           <Routes>
+            <Route path="/home" element={<HomePage />} />
             <Route path="/users" element={<UserPage />} />
             <Route path="/products" element={<ProductPage />} />
             <Route path="/orders" element={<OrderPage />} />
-            <Route path="/" element={<HomePage />} />
           </Routes>
         </div>
       </Router>
